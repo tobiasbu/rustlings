e7a6c9f539e4d3c1afa3bc6ff6cf8a7ed809d7e9
Tobias Ulrich <tobiasbulrich@gmail.com>
2020-11-15 08:12:00 -0300

    Solve exercise variables5.rs

--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -1,11 +1,12 @@
 // variables5.rs
 // Make me compile! Execute the command `rustlings hint variables5` if you want a hint :)
-
-// I AM NOT DONE
+//
+// Resolution:
+// Read about shadowing https://doc.rust-lang.org/book/ch03-01-variables-and-mutability.html#shadowing
 
 fn main() {
     let number = "T-H-R-E-E";
     println!("Spell a Number : {}", number);
-    number = 3;
+    let number = 3;
     println!("Number plus two is : {}", number + 2);
 }
