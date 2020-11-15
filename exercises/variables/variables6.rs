cba67ad119d0b277a7712f39476386c2a216f02d
Tobias Ulrich <tobiasbulrich@gmail.com>
2020-11-15 08:13:12 -0300

    Solve exercise variables6.rs

--- exercises/variables/variables6.rs
+++ exercises/variables/variables6.rs
@@ -1,9 +1,10 @@
 // variables6.rs
 // Make me compile! Execute the command `rustlings hint variables6` if you want a hint :)
+//
+// Apparently, 'const' need to have type annotation
+//
 
-// I AM NOT DONE
-
-const NUMBER = 3;
+const NUMBER: i32 = 3;
 fn main() {
     println!("Number {}", NUMBER);
 }
